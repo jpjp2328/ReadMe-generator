faaf340c64893445cc518bdefa29317f43dacb95
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-10-11 19:01:25 +1100

    questions added

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'project',
+        message: "What is your project's name",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ["MIT","APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: 'input',
+        name: 'command',
+        message: 'What command should be run to install dependencies?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What does the user need to know about contributing to the repo?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
